b3872780404bf0897e535d5d356e19b9924bd283
Yuvansri K <ysri87923@gmail.com>
2026-07-20 09:43:11 +0530

    Work.1

---
+++ src/DAY24/Happynumber.java
@@ -0,0 +1,27 @@
+package DAY24;
+
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private int getNext(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
---
+++ src/DAY24/findTheDifference.java
@@ -0,0 +1,17 @@
+package DAY24;
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int result = 0;
+
+        for (char c : s.toCharArray()) {
+            result ^= c;
+        }
+
+        for (char c : t.toCharArray()) {
+            result ^= c;
+        }
+
+        return (char) result;
+    }
+}
